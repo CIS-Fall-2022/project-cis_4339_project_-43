5c38dc2f2492e74bd3eb1ccce18d7d4e0e76b4c4
luket8032 <66449324+luket8032@users.noreply.github.com>
2022-10-03 18:20:23 -0500

    updated

--- backend/routes/organizationData.js
+++ backend/routes/organizationData.js
@@ -16,7 +16,7 @@ router.get("/", (req, res, next) => {
         }
     ).sort({ 'updatedAt': -1 }).limit(10);
 });
-/*
+
 //GET single entry by ID
 router.get("/id/:id", (req, res, next) => { 
     orgdata.find({ _id: req.params.id }, (error, data) => {
@@ -27,45 +27,7 @@ router.get("/id/:id", (req, res, next) => {
         }
     })
 });
-*/
-/*
-//GET entries based on search query
-//Ex: '...?eventName=Food&searchBy=name' 
-router.get("/search/", (req, res, next) => { 
-    let dbQuery = "";
-    if (req.query["searchBy"] === 'name') {
-        dbQuery = { eventName: { $regex: `^${req.query["eventName"]}`, $options: "i" } }
-    } else if (req.query["searchBy"] === 'date') {
-        dbQuery = {
-            date:  req.query["eventDate"]
-        }
-    };
-    orgdata.find( 
-        dbQuery, 
-        (error, data) => { 
-            if (error) {
-                return next(error);
-            } else {
-                res.json(data);
-            }
-        }
-    );
-});
 
-//GET organization for which a client is signed up
-router.get("/client/:id", (req, res, next) => { 
-    eventdata.find( 
-        { attendees: req.params.id }, 
-        (error, data) => { 
-            if (error) {
-                return next(error);
-            } else {
-                res.json(data);
-            }
-        }
-    );
-});
-*/
 
 //POST
 router.post("/", (req, res, next) => { 
@@ -80,7 +42,7 @@ router.post("/", (req, res, next) => {
         }
     );
 });
-/*
+
 //PUT
 router.put("/:id", (req, res, next) => {
     orgdata.findOneAndUpdate(
@@ -95,37 +57,7 @@ router.put("/:id", (req, res, next) => {
         }
     );
 });
-*/
-/*
-//PUT add attendee to event
-router.put("/addAttendee/:id", (req, res, next) => {
-    //only add attendee if not yet signed uo
-    orgdata.find( 
-        { _id: req.params.id, attendees: req.body.attendee }, 
-        (error, data) => { 
-            if (error) {
-                return next(error);
-            } else {
-                if (data.length == 0) {
-                    eventdata.updateOne(
-                        { _id: req.params.id }, 
-                        { $push: { attendees: req.body.attendee } },
-                        (error, data) => {
-                            if (error) {
-                                consol
-                                return next(error);
-                            } else {
-                                res.json(data);
-                            }
-                        }
-                    );
-                }
-                
-            }
-        }
-    );
-    
-});
-*/
+
+
 
 module.exports = router;
