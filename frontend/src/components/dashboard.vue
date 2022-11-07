abc978b47d72ba3db12c860861f05efb46c9eab6
luket8032 <luket8032@hotmail.com>
2022-11-07 17:37:47 -0600

    added canvas tag for chart

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -3,8 +3,14 @@
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
     </div>
+
+    <div>
+      <canvas ref="myChart"></canvas>
+    </div>
+
   </main>
 </template>
+
 <script>
 export default {
   methods: {
